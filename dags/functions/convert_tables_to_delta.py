17b81d838ab4bcdaf617659d955ac8fad7f3aa4f
Lucas Müller <lucas.mullers@gmail.com>
2023-02-26 15:32:00 -0300

    remove airflow import

--- dags/functions/convert_tables_to_delta.py
+++ dags/functions/convert_tables_to_delta.py
@@ -2,24 +2,11 @@ from pyspark.sql import SparkSession
 
 
 def create_spark_session(connection_id: str = "aws"):
-    import json
-    from pyspark.sql import SparkSession
-    from airflow.hooks.base import BaseHook
-
-    conn = BaseHook.get_connection(conn_id=connection_id)
-
-    print("Login: ", conn.login)
-    print("Password: ", conn.password)
-    print("Endpoint: ", json.loads(conn.extra)["endpoint_url"])
-
     spark = (
         SparkSession
         .builder
         .config("com.amazonaws.services.s3.enableV4", "true")
-        .config("fs.s3a.access.key", conn.login)
-        .config("fs.s3a.secret.key", conn.password)
         .config("fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
-        # .config("fs.s3a.endpoint", json.loads(conn.extra)["endpoint_url"])
         # .config("fs.s3a.connection.ssl.enabled", "false")
         .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")
         .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
